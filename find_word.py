1ec52e9f3ee801c80848fe28dc35864d599af945
Serhii Nesterenko <sy.nesterenko@gmail.com>
2023-04-25 20:35:05 +0300

    123

--- find_word.py
+++ find_word.py
@@ -2,4 +2,12 @@ import re
 
 
 def find_word(text, word):
-    pass
+    sword = word.lower()   
+    regex_name = re.compile(sword, re.IGNORECASE)  
+    # res = regex_name.findall(text)
+    return regex_name.findall(text)
+
+
+print(find_word(
+    "Guido van Rossum began working on Python in the late 1980s, as a successor to the ABC programming language, and first released it in 1991 as Python 0.9.0.",
+    "Python"))

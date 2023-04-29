98e9a6227f432031f5b4496726a26a22ff189166
Serhii Nesterenko <sy.nesterenko@gmail.com>
2023-04-29 10:09:33 +0300

    Update find_all_emails.py

--- find_all_emails.py
+++ find_all_emails.py
@@ -8,12 +8,33 @@ import re
 #     result = re.findall(pattern, text)
 #     return result
 
-def find_all_emails(text):
-    pattern = r'(?!\d)[A-Za-z][\w.]+@[A-Za-z0-9]+\.[A-Z|a-z]{2,}\b'
-    return re.findall(pattern, text)
+# def find_all_emails(text):
+#     pattern = r'(?!\d)[A-Za-z][\w.]+@[A-Za-z0-9]+\.[A-Z|a-z]{2,}\b'
+#     return re.findall(pattern, text)
 
 
 # ['['Ima.Fool@iana.org',                'first_last@iana.org', 'first.middle.last@iana.or', 'abc111@test.com']
 # ['Ima.Fool@iana.org', 'Fool@iana.org', 'first_last@iana.org', 'first.middle.last@iana.or', 'abc111@test.com']
 # 
-print(find_all_emails('Ima.Fool@iana.org Ima.Fool@iana.o 1Fool@iana.org first_last@iana.org first.middle.last@iana.or a@test.com abc111@test.com.net' ))
+# print(find_all_emails('Ima.Fool@iana.org Ima.Fool@iana.o 1Fool@iana.org first_last@iana.org first.middle.last@iana.or a@test.com abc111@test.com.net' ))
+
+# ['+380(67)777-7-77', '+380(67)777-77-77', '+380(67)777-77-78']
+# ['+380(67)777-7-771', '+380(67)777-77-77',                         '+380(67)777-77-78']
+
+# import re
+
+
+def find_all_phones(text):
+    res_match = re.findall(r"\+[380]+\([0-9]+\)[0-9]+\-[0-9]{1,}\-[0-9]{2,}", text)
+    result = []
+    for el in res_match:
+        if len(el) == 17:
+            result.append(el)
+    return result
+
+def find_all_phones(text):
+    res_match = re.findall(r"\+[380]+\([0-9]+\)[0-9-]{7}+[0-9]{2}", text)
+   
+    return res_match
+
+print(find_all_phones(" 'Irma +380(67)777-7-771 second +380(67)777-77-77 aloha a@test.com abc111@test.com.net +380(67)111-777-777+380(67)777-77-787'"))
